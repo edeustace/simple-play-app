887e61699b0d2c3352b08837a0c59f0d9551ab0e
edeustace <ed.eustace@gmail.com>
2014-05-07 19:16:14 +0100

    add procfile

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,16 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+    import java.util.Calendar
+    import java.text.SimpleDateFormat
+
+    val today = Calendar.getInstance.getTime
+    val curTimeFormat = new SimpleDateFormat("dd/MM/YY - HH:mm.ss")
+
+    val s = curTimeFormat.format(today)
+
+    Ok(views.html.index(s"$s - Your new application is ready."))
   }
 
 }
