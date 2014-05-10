956c2df36ee6509be64c015925b3156304d8c6ad
edeustace <ed.eustace@gmail.com>
2014-05-10 16:29:04 +0100

    fix runtime exception

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,16 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-
-    import java.util.Calendar
-    import java.text.SimpleDateFormat
-
-    val today = Calendar.getInstance.getTime
-    val curTimeFormat = new SimpleDateFormat("dd/MM/YY - HH:mm.ss")
-
-    val s = curTimeFormat.format(today)
-
-    Ok(views.html.index(s"$s - Your new application is ready."))
+    Ok(views.html.index(s"I'm a simple play app"))
   }
 
 }
